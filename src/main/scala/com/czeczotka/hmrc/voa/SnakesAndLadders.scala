311dd1c33a44eeee828edd6a2f0e5cee464e9f48
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2017-06-12 23:19:15 +0100

    Implement 'Token Can Move Across the Board' scenario

--- src/main/scala/com/czeczotka/hmrc/voa/SnakesAndLadders.scala
+++ src/main/scala/com/czeczotka/hmrc/voa/SnakesAndLadders.scala
@@ -1,5 +1,23 @@
 package com.czeczotka.hmrc.voa
 
+import scala.collection.mutable
+
 class SnakesAndLadders {
+
+  private var tokens = 0
+  private var positions = mutable.Map[Int, Int]().withDefaultValue(0)
+
+  def addToken(): Int = {
+    tokens = tokens + 1
+    positions += tokens -> 1
+    tokens
+  }
+
+  def position(token: Int) = positions(token)
+
+  def moveToken(token: Int, moves: Int): Unit = {
+    val current = positions(token)
+    positions(token) = current + moves
+  }
 
 }
--- src/test/scala/com/czeczotka/hmrc/voa/SnakesAndLaddersSpec.scala
+++ src/test/scala/com/czeczotka/hmrc/voa/SnakesAndLaddersSpec.scala
@@ -1,26 +1,49 @@
 package com.czeczotka.hmrc.voa
 
-import org.scalatest.FeatureSpec
+import org.scalatest.{FeatureSpec, GivenWhenThen}
 
-class SnakesAndLaddersSpec extends FeatureSpec {
+class SnakesAndLaddersSpec extends FeatureSpec with GivenWhenThen {
 
   feature("Moving Your Token") {
+
     scenario("Token Can Move Across the Board") {
       info("As a player")
       info("I want to be able to move my token")
       info("So that I can get closer to the goal")
-      pending
+
+      Given("the game is started")
+      val game = new SnakesAndLadders
+
+      When("the token is placed on the board")
+      val token = game.addToken()
+
+      Then("the token is on square 1")
+      assert(game.position(token) === 1)
+
+      When("the token is moved 3 spaces")
+      game.moveToken(token, 3)
+
+      Then("the token is on square 4")
+      assert(game.position(token) === 4)
+
+      When("then it is moved 4 spaces")
+      game.moveToken(token, 4)
+
+      Then("the token is on square 8")
+      assert(game.position(token) === 8)
     }
-    scenario("Player Can Win the Game") {
-      info("As a player")
-      info("I want to be able to win the game")
-      info("So that I can gloat to everyone around")
-      pending
-    }
+
     scenario("Moves Are Determined By Dice Rolls") {
       info("As a player")
       info("I want to move my token based on the roll of a die")
       info("So that there is an element of chance in the game")
+      pending
+    }
+
+    scenario("Player Can Win the Game") {
+      info("As a player")
+      info("I want to be able to win the game")
+      info("So that I can gloat to everyone around")
       pending
     }
   }
